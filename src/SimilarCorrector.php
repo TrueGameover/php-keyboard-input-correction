f8364a3dd59222ff7facd7ebeb4cc01d0602aae9
Aleksey Ivachev <truegameover@gmail.com>
2018-12-04 20:06:00 +0700

    корректор схожих символов.

---
+++ src/SimilarCorrector.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Алексей
+ * Date: 04.12.2018
+ * Time: 19:26
+ */
+
+namespace KeyboardInputCorrection;
+
+use KeyboardInputCorrection\dictionaries\SimilarDictionary;
+
+/**
+ * Correct similar chars between languages
+ * Class SimilarCorrector
+ * @package KeyboardInputCorrection
+ */
+class SimilarCorrector extends InputCorrection {
+
+    /**
+     * Transliteration
+     * @param string $input
+     * @param int $targetLanguage
+     * @param string $encode
+     * @return string
+     * @throws \InvalidArgumentException
+     */
+    public function correct(string $input, int $targetLanguage = self::LANGUAGE_RU, string $encode = self::DEFAULT_ENCODE): string {
+
+        if( $encode !== self::DEFAULT_ENCODE && !$this->isEncodeSupported($encode) ) {
+
+            throw new \InvalidArgumentException($encode . ' unknown');
+        }
+
+        $input = $this->prepare($input, $encode);
+
+        $table = $this->getSimilarTable($targetLanguage);
+
+        foreach( $table as $key => $value ) {
+
+            $modified = mb_eregi_replace($key, $value, $input);
+
+            if( $modified !== false ) {
+
+                $input = (string)$modified;
+            }
+        }
+
+        return $this->finish($input, $encode);
+    }
+
+    protected function getSimilarTable(int $language): array {
+
+        switch($language) {
+
+            case self::LANGUAGE_RU:
+                return SimilarDictionary::for_ru();
+        }
+
+        return [];
+    }
+}
